a97b9c02b4ceb9aafe4dde6deed7110a36746153
duccuchuoi <duccuchuoi1994@gmail.com>
2014-07-04 16:20:24 -0700

    first commit

---
+++ Beer.java
@@ -0,0 +1,18 @@
+// in thong tin ca nhan ho va ten, ma sinh vien, lop, username github, mail va in 20 dong chu nhu yeu cau 
+public class Beer{
+    public static void main ( String [] args){
+	    System.out.println(" \t Ho va ten: Le Quang Duc");
+		System.out.println(" \t Ma sinh vien: 12020098");
+		System.out.println(" \t Lop: K57CD ");
+		System.out.println(" \t username: duccuchuoi");
+		System.out.println(" \t Mail: duccuchuoi1994@gmail.com \n\n");
+		/* su dung vong for de in 20 dong 
+		gom bottles of beer on the wall, bottles of beer 
+		 Take one down, pass in around*/
+		for(int i=9;i>0;i--){
+		    System.out.println(     i + " bottles of beer on the wall,  bottles of beer");
+		    System.out.println(" Take one down, pass in around");
+		}
+		System.out.println(" No more bottles of beer on the wall.");
+	}
+}
